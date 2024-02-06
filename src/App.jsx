3a0efcfc89a0bb87996fc352ac28a0c0a5627d16
KY <rudyeon@naver.com>
2024-02-06 14:40:58 +0900

    첫번째문제

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { minus, plus } from "./redux/modules/calculator";
 
 function App() {
+  const dispatch = useDispatch();
+  const totalNumber = useSelector((state) => state.calculator.totalNumber);
+  const onChangeHandler = (event) => {
+    setNumber(+event.target.value);
+  };
+  const plusNumber = () => {
+    dispatch(plus(number));
+  };
+  const minusNumber = () => {
+    dispatch(minus(number));
+  };
+  const [number, setNumber] = useState();
+
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input value={number} onChange={onChangeHandler} /> 만큼을
+        <button onClick={plusNumber}>더할게요</button>
+        <button onClick={minusNumber}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{totalNumber}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,42 @@
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
+export const plus = (payload) => {
+  return {
+    type: PLUS,
+    payload,
+  };
+};
+
+export const minus = (payload) => {
+  return {
+    type: MINUS,
+    payload,
+  };
+};
+
 // 초기 상태값
 const initialState = {
-  number: 0,
+  totalNumber: 0,
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS:
+      return {
+        ...state,
+        totalNumber: state.totalNumber + action.payload,
+      };
+    case MINUS:
+      return {
+        ...state,
+        totalNumber: state.totalNumber - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
